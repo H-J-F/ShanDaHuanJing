3f8c5a13220f4826aa3cccbbb746a3a39f0f2e48
11691 <1169111974@qq.com>
2017-04-13 10:24:16 +0800

    app

--- app/src/main/java/com/example/a11691/shandahuanjing/DataShow.java
+++ app/src/main/java/com/example/a11691/shandahuanjing/DataShow.java
@@ -1,13 +1,43 @@
 package com.example.a11691.shandahuanjing;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class DataShow extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setNoTitle();
         setContentView(R.layout.data_show_layout);
+
+        TextView any_data = (TextView) findViewById(R.id.any_data);
+
+        Bundle get_data_bundle = this.getIntent().getExtras();
+        String title = get_data_bundle.getString("title");
+        String data = get_data_bundle.getString("value");
+        this.setTitle(title);
+        any_data.setText(data);
+    }
+
+    private void setNoTitle(){
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+            window.setNavigationBarColor(Color.TRANSPARENT);
+        }
     }
 }
